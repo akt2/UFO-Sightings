900ba7bd14a1e9c6c6cf9af02600e4276cc7ada9
akt2 <atwitchellheyne@gmail.com>
2019-05-21 19:04:20 -0400

    more

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,3 +14,12 @@ tableData.forEach(function(ufoSight) {
         cell.text(value);
     })
 });
+
+var submit=d3.select('#filter-btn');
+submit.on('click',function() {
+    d3.event.preventDefault();
+    var input=d3.select('#datetime');
+    var inpvalue=input.property('value');
+    var filtered=tableData.filter(tableData => tableData.datetime === inpvalue);
+    console.log(filtered);
+});
